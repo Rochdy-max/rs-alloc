7eb2f9936874a5945f9945f89a7c52365a9c1d9a
Rochdy-max <rochdy.bachabi@epitech.eu>
2024-11-06 16:21:27 +0100

    add unimplemented methods for GlobalAlloc trait implementation for MyAlloc struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,38 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+// set no_std attribute only if std is not set as feature
+#![cfg_attr(not(feature = "std"), no_std)]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#![allow(unused_variables)]
+#![allow(dead_code)]
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+
+#[cfg(feature = "myalloc")]
+pub mod myalloc {
+    use core::alloc::GlobalAlloc;
+    
+    pub struct MyAlloc;
+    
+    unsafe impl GlobalAlloc for MyAlloc {
+        unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
+            unimplemented!()
+        }
+        
+        unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {
+            unimplemented!()
+        }
     }
 }
+
+// pub fn add(left: u64, right: u64) -> u64 {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
